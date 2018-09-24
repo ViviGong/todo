63d57d5f113aead70a2cd0c454119f440c84df81
ViviGong <gongweiuibe@gmail.com>
2018-09-24 11:42:36 -0400

    submit

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ function addNewList() {
 
 function addListItem() {
     console.log('hello world');
+    var list = document.getElementById("grocery-list");
+    var itemInput = document.getElementById("new-list-item")
+    var newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+
+function deleteListItem(item) {
+    item.parentNode.removeChild(item);
 }
